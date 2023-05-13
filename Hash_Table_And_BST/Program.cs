4d2cae71a399710e81fbd35d047341402332ebeb
Harshita <chandaliyaharshita222@gmail.com>
2023-05-13 22:29:07 +0530

    Added Welcome Message

---
+++ Hash_Table_And_BST/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hash_Table_And_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table and Binary Search Tree Program!");
+        }
+    }
+}
